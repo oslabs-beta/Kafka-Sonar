d4d318a5d0a95ac1446a42bb914793bd25254726
Michael Way <mway@Michaels-Mac-mini.local>
2023-07-13 00:09:53 -0400

    refactored test middleware to take an input of host:port pairs

--- server/controllers/promController.ts
+++ server/controllers/promController.ts
@@ -24,7 +24,43 @@ const write = (host, port) => {
   });
 }
 
-write('kafka99', '9999');
+// write('kafka99', '9999');
+
+const writeMany = (jmxArr) => {
+  // shape of user input
+  // req.body.jmx = [{port, host}, {port, host}, ...]
+
+  // create targets array based on user input
+  const targets = JSON.stringify(jmxArr.map(obj => {
+    const { host, port } = obj;
+    return `${host}:${port}`;
+  }));
+
+  const ymlString = `
+  global:
+    scrape_interval: 15s
+  
+  rule_files:
+  
+  scrape_configs:
+    - job_name: "kafka"
+  
+      static_configs:
+        - targets: ${targets}`
+  const data = new Uint8Array(Buffer.from(ymlString));
+  // @ts-ignore
+  fs.writeFileSync('./demo-cluster/configs/prometheus/test.yml', data, (err: undefined) => {
+    if (err) console.log('ERROR', err);
+    console.log('the file has been saved!');
+  });
+}
+
+const test = [
+  { host: 'kafka1', port: '9999' },
+  { host: 'kafka2', port: '9999' },
+  { host: 'kafka3', port: '9999' },
+]
+writeMany(test);
 
 // now all we need to do is figure out how to spin up docker containers on an api call
 // middleware flow should be ->

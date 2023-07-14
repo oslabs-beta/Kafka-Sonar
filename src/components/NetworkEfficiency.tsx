8eb5b30c2fd4fdfd42326c14beb0f166f4e1db1c
unatarajan <38731992+unatarajan@users.noreply.github.com>
2023-07-14 00:13:55 -0400

    Moved Connect component into App; commented out metrics that were not working or that we were unable to query using PromQL; reorganized remaining cluster metrics and increased widths for time series graphs

--- src/components/NetworkEfficiency.tsx
+++ src/components/NetworkEfficiency.tsx
@@ -1,7 +1,7 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Skeleton from '@mui/material/Skeleton';
+// import Skeleton from '@mui/material/Skeleton';
 
 export default function NetworkEfficiency() {
   return (
@@ -11,44 +11,45 @@ export default function NetworkEfficiency() {
       </Typography>
       <Box component="main" sx={{ display: 'flex', flexGrow: 1, p: 3 }}>
         <Grid container spacing={{ xs: 1, sm: 1, md: 1 }}>
-          <Grid item xs={12} sm={12} md={12}>
+          <Grid item xs={8} sm={8} md={8}>
             {/* Avg E2E req handling time (graph) */}
             <iframe src="http://localhost:3000/d-solo/aRNaJwOmk/kafka-broker-performance-and-latency?orgId=1&refresh=5s&panelId=1"></iframe>{' '}
           </Grid>
-        </Grid>
-      </Box>
-      <Box component="main" sx={{ display: 'flex', flexGrow: 1, p: 3 }}>
-        <Grid container spacing={{ xs: 1, sm: 1, md: 1 }}>
-          <Grid item xs sm md>
+          <Grid item xs={2} sm={2} md={2}>
             {/* Producer request rate OR req to server (broker?) / sec (graph) */}
             <iframe src="http://localhost:3000/d-solo/e-6AJQOik/kafka-cluster-global-healthcheck?orgId=1&refresh=5s&panelId=39"></iframe>{' '}
           </Grid>
-          <Grid item xs={4} sm={4} md={4}>
-            {/* Broker response rate OR res to consumers / sec (graph) */}
-            <Skeleton animation={false} variant="rectangular" height={160} />
-          </Grid>
-          <Grid item xs sm md>
+          <Grid item xs={2} sm={2} md={2}>
             {/* Consumer fetch rate OR req to server (broker?) / sec (graph) */}
             <iframe src="http://localhost:3000/d-solo/e-6AJQOik/kafka-cluster-global-healthcheck?orgId=1&refresh=5s&panelId=40"></iframe>
           </Grid>
         </Grid>
       </Box>
-      <Box component="main" sx={{ display: 'flex', flexGrow: 1, p: 3 }}>
+
+      {/* <Box component="main" sx={{ display: 'flex', flexGrow: 1, p: 3 }}>
         <Grid container spacing={{ xs: 1, sm: 1, md: 1 }}>
+
+          // Following were metrics that didn't work while running demo cluster (might be hard-coded)
+          <Grid item xs={4} sm={4} md={4}>
+            // Requests in-flight (awaiting responses) (graph)
+            <iframe src="http://localhost:3000/d-solo/aRNaJwOmk/kafka-broker-performance-and-latency?orgId=1&refresh=5s&panelId=10"></iframe>
+          </Grid>
+
+          // Following were metrics we were unable to query using PromQL due to lack of documentation / bandwidth to research:
           <Grid item xs sm md>
-            {/* Failed producer request rate OR failed req to server / sec (graph) */}
+            // Failed producer request rate OR failed req to server / sec (graph)
             <Skeleton animation={false} variant="rectangular" height={160} />{' '}
           </Grid>
           <Grid item xs={4} sm={4} md={4}>
-            {/* Requests in-flight (awaiting responses) (graph) */}
-            <iframe src="http://localhost:3000/d-solo/aRNaJwOmk/kafka-broker-performance-and-latency?orgId=1&refresh=5s&panelId=10"></iframe>
+            // Broker response rate OR res to consumers / sec (graph)
+            <Skeleton animation={false} variant="rectangular" height={160} />
           </Grid>
           <Grid item xs sm md>
-            {/* Failed fetch request rate OR req to server (broker?) / sec (graph) */}
+            // Failed fetch request rate OR req to server (broker?) / sec (graph)
             <Skeleton animation={false} variant="rectangular" height={160} />
           </Grid>
         </Grid>
-      </Box>
+      </Box> */}
     </>
   );
 }
--- src/components/PartitionStats.tsx
+++ src/components/PartitionStats.tsx
@@ -1,7 +1,7 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Skeleton from '@mui/material/Skeleton';
+// import Skeleton from '@mui/material/Skeleton';
 
 export default function PartitionStats() {
   return (
@@ -11,15 +11,15 @@ export default function PartitionStats() {
       </Typography>
       <Box component="main" sx={{ display: 'flex', flexGrow: 1, p: 3 }}>
         <Grid container spacing={{ xs: 1, sm: 1, md: 1 }}>
-          <Grid item xs sm md>
+          <Grid item xs={4} sm={4} md={4}>
             {/* Total leaders (stat) */}
             <iframe src="http://localhost:3000/d-solo/e-6AJQOik/kafka-cluster-global-healthcheck?orgId=1&refresh=5s&panelId=14"></iframe>
           </Grid>
-          <Grid item xs sm md>
+          <Grid item xs={4} sm={4} md={4}>
             {/* Total replicas (stat) */}
             <iframe src="http://localhost:3000/d-solo/e-6AJQOik/kafka-cluster-global-healthcheck?orgId=1&refresh=5s&panelId=15"></iframe>
           </Grid>
-          <Grid item xs sm md>
+          <Grid item xs={2} sm={2} md={2}>
             {/* Offline replicas (stat) */}
             <iframe src="http://localhost:3000/d-solo/e-6AJQOik/kafka-cluster-global-healthcheck?orgId=1&refresh=5s&panelId=32"></iframe>
           </Grid>
@@ -27,16 +27,22 @@ export default function PartitionStats() {
             {/* URPs (stat) */}
             <iframe src="http://localhost:3000/d-solo/e-6AJQOik/kafka-cluster-global-healthcheck?orgId=1&refresh=5s&panelId=33"></iframe>
           </Grid>
+        </Grid>
+      </Box>
+
+      {/* Following were metrics we were unable to query using PromQL due to lack of documentation / bandwidth to research: */}
+      {/* <Box component="main" sx={{ display: 'flex', flexGrow: 1, p: 3 }}>
+        <Grid container spacing={{ xs: 1, sm: 1, md: 1 }}>
           <Grid item xs sm md>
-            {/* In-sync Replicas (stat) */}
+            // In-sync Replicas (stat)
             <Skeleton animation={false} variant="rectangular" height={160} />
           </Grid>
           <Grid item xs sm md>
-            {/* Out-of-sync Replicas (stat) */}
+            // Out-of-sync Replicas (stat)
             <Skeleton animation={false} variant="rectangular" height={160} />
           </Grid>
         </Grid>
-      </Box>
+      </Box> */}
     </>
   );
 }
